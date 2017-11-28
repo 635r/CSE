d3a6bd01b5481855f2cbca28bdbc8cbb7ead93a4
635r <31050392+635r@users.noreply.github.com>
2017-11-28 15:07:23 -0800

    adding previous notes
    11/28/17

---
+++ notes.py
@@ -0,0 +1,29 @@
+print("hello world")
+
+#Alek Ledesma
+
+# ctrl+/ to toggle between comments
+
+print(3+12)
+print(3**3)
+
+print("see if you can figure this out")
+print(13%12)
+print(5%3)
+print(103%100)
+print(100%3)
+
+# Variables
+car_name = 'Weibe Mobile'
+car_type = 'lambroghini sesto elemento'
+car_cylinder = 8
+car_mpg = 9000.1
+
+# Inline Printing
+print('my car is the %s. It is a %s'% (car_name, car_type))
+
+# Taking Input
+name = input ('The big bamboozle')
+print('Hello %s.' % name)
+age = input ("14 ")
+print("woag, %s." % age)
