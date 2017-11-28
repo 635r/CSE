90512010c12b80e53cf2d1ffa70e49c1a3cdf5f8
635r <635r@wetigers.org>
2017-11-28 15:41:16 -0800

    added functions
    11/28/17

--- notes.py
+++ notes.py
@@ -1,29 +1,53 @@
-print("hello world")
+# print("hello world")
+#
+# #Alek Ledesma
+#
+# # ctrl+/ to toggle between comments
+#
+# print(3+12)
+# print(3**3)
+#
+# print("see if you can figure this out")
+# print(13%12)
+# print(5%3)
+# print(103%100)
+#
+# # Variables
+# car_name = 'Weibe Mobile'
+# car_type = 'lambroghini sesto elemento'
+# car_cylinder = 8
+# car_mpg = 9000.1
+#
+# # Inline Printing
+# print('my car is the %s. It is a %s'% (car_name, car_type))
+#
+# # Taking Input
+# name = input ('The big bamboozle')
+# print('Hello %s.' % (name))
+# age = input ("14 ")
+# print ("wow %s " % (age))
 
-#Alek Ledesma
+# Change file
 
-# ctrl+/ to toggle between comments
+def print_hw():
+    print("Hello World")
 
-print(3+12)
-print(3**3)
+print_hw()
 
-print("see if you can figure this out")
-print(13%12)
-print(5%3)
-print(103%100)
-print(100%3)
 
-# Variables
-car_name = 'Weibe Mobile'
-car_type = 'lambroghini sesto elemento'
-car_cylinder = 8
-car_mpg = 9000.1
+def say_hi(name):
+    print ("Hello %s." % name)
+    print("I hope you have a fantastic day.")
 
-# Inline Printing
-print('my car is the %s. It is a %s'% (car_name, car_type))
 
-# Taking Input
-name = input ('The big bamboozle')
-print('Hello %s.' % name)
-age = input ("14 ")
-print("woag, %s." % age)
+say_hi ("The Big Bamboozle")
+
+
+def birthday(age):
+   age += 1 # age = age + 1
+
+say_hi ("The Big Bamboozle")
+print ("The Big Bamboozle is 14. Next year he'll be 15")
+
+birthday(15)
+
