cfd236aa4d128ee721675354b8e5efad43dd1631
635r <635r@wetigers.org>
2017-12-07 15:42:22 -0800

    12/7/17
    finished gussingame

--- Guessgame.py
+++ Guessgame.py
@@ -1,18 +1,26 @@
 import random
 
-
 number = random.randint(1, 50)
-print(number)
 
-right_guess = number
+right_guess = False
+
+turns_left = 5
+
 
 
 while turns_left > 0 and right_guess is False:
-    answer = int(input(yeet))
-    if answer == right_guess
-        return("NOICE")
-    if answer >
-    print(number == str(random.randint(1, 50)))
+    answer = int(input("try and guess"))
+    if answer == number:
+        print("NOICE")
+        right_guess = True
+    elif answer > number:
+        print("lower m9")
+        turns_left -= 1
+    elif answer < number:
+        print("higher m8")
+        turns_left -= 1
 
-turnd_left: 5
+
+if turns_left is 0:
+    print ("soory you got hecked. This was the number %s." % number)
 
