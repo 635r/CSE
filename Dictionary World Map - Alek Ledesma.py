cc669464112d756c24e6cd9484119c87bafc1e91
635r <31050392+635r@users.noreply.github.com>
2018-02-14 15:40:11 -0800

    2/14/18
    made the contrloer and fixed the map

--- Dictionary World Map - Alek Ledesma.py
+++ Dictionary World Map - Alek Ledesma.py
@@ -1,12 +1,13 @@
 world_map = {
-    "WESTHOUSE": {
-        "NAME": "WEST OF HOME",
-        "DESCRIPTION": "All of your friend's houses lie east of your home(which is were you are)"
+    "HOME": {
+        "NAME": "HOME",
+        "DESCRIPTION": "All of your friend's houses lie east of your home(which is were you are)",
         "PATHS": {
             "NORTH": "NORTHHOUSE",
             "SOUTH": "SOUTHHOUSE",
-            "EAST": "EASTHOUSE"
-        }
+            "EAST": "EASTHOUSE",
+            "WEST": "WESTHOUSE",
+         }
     },
     "SOUTHHOUSE": {
         "NAME": "SOUTH OF HOME",
@@ -20,4 +21,27 @@ world_map = {
         "NAME": "NORTH OF HOME",
         "DESCRIPTION": "To the North are a bunch of BIG ROCKS, a BEE TREE, and the NORTHPOLE"
     },
+    "WESTHOUSE": {
+        "NAME": "WEST OF HOME",
+        "DESCRIPTION": "fub"
+    }
 }
+
+current_node = world_map["HOME"]
+directions = ["NORTH", "SOUTH", "EAST", "WEST"]
+
+while True:
+    print(current_node["NAME"])
+    print(current_node["DESCRIPTION"])
+    command = input (">_ ")
+    if command == "quit":
+        quit(0)
+    if command in directions:
+        try:
+            name_of_node = current_node["PATHS"][command]
+            current_node = world_map[name_of_node]
+        except KeyError:
+            print("You can't go that way boi")
+    else:
+        print("I don't get it")
+        print()
