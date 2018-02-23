858c5cb46051ab809b7e651d154d4ba2e920c169
635r <31050392+635r@users.noreply.github.com>
2018-02-23 15:40:34 -0800

    2/23/18
    added what I think is a controler

---
+++ Items - Alek Ledesma.py
@@ -0,0 +1,31 @@
+items = {
+    "HOME": {
+        "NAME": "HOME",
+        "DESCRIPTION": "All of your friend's houses lie east of your home(which is were you are)",
+        "PATHS": {
+            "NORTH": "NORTHHOUSE",
+            "SOUTH": "SOUTHHOUSE",
+            "EAST": "EASTHOUSE",
+            "WEST": "WESTHOUSE",
+         }
+    },
+    "STICK": {
+        "NAME": "Stick",
+        "DESCRIPTION": "A simple stick that might help you"
+    },
+    "POOHBEAR": {
+        "NAME": "Winnie the Pooh",
+        "DESCRIPTION": "a stuffed Winnie the Pooh doll"
+    },
+    "BOOK": {
+        "NAME": "A.A.MILNE",
+        "DESCRIPTION": "A book by someone named A.A Milne"
+    },
+    "REASON": {
+        "NAME": "Reason",
+        "DESCRIPTION": "something you should always have"
+    },
+    "COMMONSENS": {
+        "NAME": "Common Sense",
+        "DESCRIPTION": "Something you should always use"
+    },
---
+++ Mr.Weibe - World Map OOP.py
@@ -0,0 +1,17 @@
+class Room(object):
+    def __init__(self, name, north, south, east, west):
+        self.name = name
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
+
+    def move(self, direction):
+        global current_node
+        current_node = globals()[getattr(self, direction)]
+
+home = Room("Home", )
+west_home = Room("West of Home", "west_home")
+north_home = Room("North of Home", None)
+east_home = Room("East of Home", )
+south_home = Room("South of Home",)
