21748fd41917ee88f7c84ca01313779f1db7f259
635r <31050392+635r@users.noreply.github.com>
2018-03-16 15:41:31 -0700

    3/16/18
    working on item

--- Items - Alek Ledesma.py
+++ Items - Alek Ledesma.py
@@ -1,7 +1,22 @@
-class Item(object)
+class item(object):
     def __init__(self, boost, health, weapon, key):
         self.boost = boost
         self.health = health
         self.weapon = weapon
         self.key = key
 
+class boost(item):
+    def __init__(self, hp, attack, deffense):
+        self.hp = hp
+        self.attack = attack
+        self.deffense = deffense
+
+class hp (boost):
+    def __init__(self, turn3, turn6):
+        self.turn3 = turn3
+        self.turn6 = turn6
+
+class attack (boost):
+    def __init__(self, turn3, turn6):
+        self.turn3 = turn3
+        self.turn6 = turn6
