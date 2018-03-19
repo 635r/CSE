8934e5f5d0dfc6b75adea527f14ab272ed97bb82
635r <31050392+635r@users.noreply.github.com>
2018-03-19 15:40:12 -0700

    3/19/18
    got alot done on the item map

--- Items - Alek Ledesma.py
+++ Items - Alek Ledesma.py
@@ -1,15 +1,16 @@
 class item(object):
-    def __init__(self, boost, health, weapon, key):
+    def __init__(self, boost, heal, weapon, key):
         self.boost = boost
-        self.health = health
+        self.heal = heal
         self.weapon = weapon
         self.key = key
 
+
 class boost(item):
-    def __init__(self, hp, attack, deffense):
+    def __init__(self, hp, attack, defense):
         self.hp = hp
         self.attack = attack
-        self.deffense = deffense
+        self.defense = defense
 
 class hp (boost):
     def __init__(self, turn3, turn6):
@@ -20,3 +21,52 @@ class attack (boost):
     def __init__(self, turn3, turn6):
         self.turn3 = turn3
         self.turn6 = turn6
+
+class defense (boost):
+    def __init__(self, turn3, turn6):
+        self.turn3 = turn3
+        self.turn6 = turn6
+
+
+class heal (item):
+    def __init__(self, hp, defense):
+        self.hp = hp
+        self.defense = defense
+
+class hp (heal):
+    def __init__(self, ten, fifteen):
+        self.ten = ten
+        self.fifteen = fifteen
+
+class defense (heal):
+    def __init__(self, restore):
+        self.restore = restore
+
+
+class weapon (item):
+    def __init__(self, sword, gun, stick):
+        self.sword = sword
+        self. gun = gun
+        self. stick = stick
+
+class gun (weapon):
+    def __init__(self, AR, revolver, pistol):
+        self.AR = AR
+        self.revolver = revolver
+        self.pistol = pistol
+
+class sword (weapon):
+    def __init__(self, good, bad):
+        self.good = good
+        self.bad = bad
+
+class stick (weapon):
+    def __init__(self, big, small):
+        self.big = big
+        self.small = small
+
+
+class key (item):
+    def __init__(self, rock, security):
+        self.rock = rock
+        self.security = security
